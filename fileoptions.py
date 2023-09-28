9a821516a034674337428cd4ab052cdefff9e04d
leow20@uw.edu <leow20@uw.edu>
2023-09-27 23:49:11 -0700

    Code without comments

--- fileoptions.py
+++ fileoptions.py
@@ -1,11 +1,11 @@
 import tkinter.filedialog as tkf
 
-def uploadFile(filelist, ui):
+def uploadFile(filelist, ui, style):
    pathTuple = tkf.askopenfilenames(title = "Select a file", filetypes = [("PDF Files", "*.pdf")])
    pathList = list(pathTuple)
    for path in pathList:
       filelist.append(path)
-      ui.fileDisplay(path)
+      ui.fileDisplay(path, style)
 
 def removeFile(filelist, pdfname, pairList, ui):
    for path in filelist:
@@ -24,3 +24,4 @@ def removeWidget(pairList, index, ui):
       label.grid(row = i)
       button.grid(row = i)
       button["command"] = lambda filename = ui.fileList[i]: removeFile(ui.fileList, filename, ui.label_button_pairs, ui)
+      ui.onCanvasConfigure(None)
--- gui.py
+++ gui.py
@@ -8,23 +8,18 @@ class ProgramUI:
 
     fileList = []
     label_button_pairs = []
-    row_number = 0
 
     def __init__(self, master):
        master.title("Leo's PDF Merger")
        self.frameSetup(master)
 
     def frameSetup(self, master):
-       master.geometry("800x350")
-       master.minsize(800, 350)
+       style = ttk.Style()
+       master.minsize(600, 350)
        master.resizable(False, False)
        self.introCreate(master)
-       self.displayFunctionality(master)
-       self.functionality(master)
-       ttk.Button(master, command = lambda: print(self.fileList), text = "Show list").pack()
-       ttk.Button(master, command = lambda: print(self.label_button_pairs), text = "Show widget").pack()
-       ttk.Button(master, command = lambda: print(len(self.fileList)), text = "Show list length").pack()
-       ttk.Button(master, command = lambda: print(len(self.label_button_pairs) - 1), text = "Pair len").pack()
+       self.displayFunctionality(master, style)
+       self.functionality(master, style)
 
     # The UI for the program introduction
     def introCreate(self, master):
@@ -35,15 +30,15 @@ class ProgramUI:
        self.intro.pack(fill = "both", expand = True)
 
     # The UI for the functionality
-    def functionality(self, master):
-       self.buttonFrame = ttk.Frame(master, relief = "ridge", height = 100, width = 100)
-       self.buttonFrame.pack(side = "left", expand = 1, padx = 10, pady = 10)
-       self.uploadFunctionality(self.buttonFrame)
+    def functionality(self, master, style):
+       self.buttonFrame = ttk.Frame(master, height = 100, width = 100)
+       self.buttonFrame.pack(side = "left", expand = 1, pady = 20)
+       self.uploadFunctionality(self.buttonFrame, style)
        self.mergeFunctionality(self.buttonFrame)
 
     # The UI for the upload button
-    def uploadFunctionality(self, master):
-       self.uploadButton = ttk.Button(master, command = lambda: fo.uploadFile(self.fileList, self), text = "Upload")
+    def uploadFunctionality(self, master, style):
+       self.uploadButton = ttk.Button(master, command = lambda: fo.uploadFile(self.fileList, self, style), text = "Upload")
        self.uploadButton.grid(row = 2, column = 1)
 
     # The UI for the merge button
@@ -51,19 +46,44 @@ class ProgramUI:
        self.mergeButton = ttk.Button(master, command = lambda: pdf.mergePDF(self.fileList, self.displayFrame), text = "Merge")
        self.mergeButton.grid(row = 2, column = 2)
 
-    def displayFunctionality(self, master):
-       self.displayFrame = ttk.Frame(master)
-       self.displayFrame.pack(padx = 20, pady = 20)
+    def displayFunctionality(self, master, style):
+       self.containerFrame = ttk.Frame(master)
+       self.containerFrame.pack(fill = "both", expand = True, padx = 20)
 
-    def fileDisplay(self, filename):
+       self.displayCanvas = tk.Canvas(self.containerFrame, bg = "white", width = 300, height = 200)
+       self.displayScrollBar = tk.Scrollbar(self.containerFrame, orient = "vertical", command = self.displayCanvas.yview)
+       self.displayCanvas.configure(yscrollcommand = self.displayScrollBar.set)
+
+       style.configure("Display.TFrame", background = "white")
+       self.displayFrame = ttk.Frame(self.displayCanvas, style = "Display.TFrame")
+       self.displayCanvas.create_window(250, 0, window = self.displayFrame, anchor = "n")
+
+       self.displayCanvas.bind("<Configure>", self.onCanvasConfigure)
+       self.displayCanvas.bind("<MouseWheel>", self._on_mousewheel)
+
+       self.displayCanvas.pack(side = "left", fill = "both", expand = True, padx = 20)
+       self.displayScrollBar.pack(side = "right", fill = "y")
+
+    def fileDisplay(self, filename, style):
        pdfname = re.search("[^\/]*$", filename).group()
-       self.createLabelButtonPair(self.displayFrame, pdfname)
+       self.createLabelButtonPair(self.displayFrame, pdfname, style)
 
-    def createLabelButtonPair(self, master, pdfname):
+       self.displayFrame.update_idletasks()
+       self.displayCanvas.configure(scrollregion = self.displayCanvas.bbox("all"))
+
+    def createLabelButtonPair(self, master, pdfname, style):
+       style.configure("PDF.TLabel", background = "white")
        self.row_number = len(self.label_button_pairs)
-       self.fileLabel = ttk.Label(master, text = pdfname)
+       self.fileLabel = ttk.Label(master, text = pdfname, style = "PDF.TLabel")
        self.fileLabel.grid(row = self.row_number, column = 0, padx = 5, pady = 5, sticky = "w")
        self.removeButton = ttk.Button(master, command = lambda: fo.removeFile(self.fileList, pdfname,
        self.label_button_pairs, self), text = "Remove")
        self.removeButton.grid(row = self.row_number, column = 1, padx = 5, pady = 5)
        self.label_button_pairs.append((self.fileLabel, self.removeButton))
+
+    def onCanvasConfigure(self, event):
+       self.displayCanvas.configure(scrollregion = self.displayCanvas.bbox("all"))
+
+    def _on_mousewheel(self, event):
+       if self.displayCanvas.bbox("all")[3] > self.displayCanvas.winfo_height():
+        self.displayCanvas.yview_scroll(-1 * (event.delta//120), "units")
